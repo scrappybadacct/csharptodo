635df2e5e6d9146b2da05b07d4cfab29a84e9438
Conor Fleming <conorfleming@outlook.com>
2019-07-01 14:11:54 -0500

    fixed nullable param to overloaded const in todoitem

--- src/Models/TodoItem.cs
+++ src/Models/TodoItem.cs
@@ -5,14 +5,21 @@ namespace Models
   public class TodoItem
   {
     public long Id { get; private set; }
-    public DateTime? TimeStamp { get; private set; }
+    public DateTime TimeStamp { get; private set; }
     public string ItemText { get; private set; }
 
-    public TodoItem(long id, string text, DateTime? time = null)
+    public TodoItem(long id, string text)
     {
       Id = id;
       ItemText = text;
-      TimeStamp = time != null ? time : DateTime.Now;
+      TimeStamp = DateTime.Now;
+    }
+
+    public TodoItem(long id, string text, DateTime time)
+    {
+      Id = id;
+      ItemText = text;
+      TimeStamp = time;
     }
 
     public void Update(string txt)
